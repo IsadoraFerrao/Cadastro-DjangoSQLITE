ed9790ca902228056a0807e9aa98b89e23174591
IsadoraFerrao <isadora-gf@hotmail.com>
2024-03-18 21:38:25 -0300

    aula01

---
+++ cadastro/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CadastroConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cadastro'
---
+++ cadastro/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+from cadastro.forms import CadastroForm
+
+# Create your views here.
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def cadastro(request):
+    sucesso = False 
+    if request.method == 'GET':
+        form = CadastroForm()
+    else:
+        form = CadastroForm(request.POST)
+        if form.is_valid(): 
+            sucesso = True 
+    contexto = {
+        'form': form,
+        'sucesso': sucesso
+    }
+    return render(request, 'cadastro.html', contexto)
